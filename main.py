e255b12f002ec0d8db49c37cb4ee77216a8aaf42
kosvish <mr.kocta@bk.ru>
2023-03-15 10:36:42 +0300

    feat(create): asking func and json object question

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ questions = [
     {
         "question": "Какой газ является основным компонентом воздуха?",
         "answers": ["азот", 'кислород', "углекислый газ", "аргон"],
-        "correct_answer": "10"
+        "correct_answer": "азот"
     },
     {
         "question": "Что из перечисленного является нечетным числом?",
@@ -29,3 +29,33 @@ def ask_question(question, answers):
         user_answer = input("Ваш ответ: ")
         if user_answer.isdigit() and int(user_answer) in range(1, len(answers) + 1):
             return answers[int(user_answer) - 1]
+
+
+print(f"""
+Привествую всех в игре кто хочет стать миллионером!
+Суть игры проста у нас есть {len(questions)} вопроса 
+Игроку лишь нужно ответь на все них правильно!
+За каждый правильный ответ игрок получает 100$
+""")
+
+
+def play_game(questions):
+    random.shuffle(questions)
+
+    score = 0
+    current_question = 0
+
+    while current_question < len(questions) and ask_question(questions[current_question]["question"],
+                                                             questions[current_question]["answers"]) == \
+            questions[current_question]["correct_answer"]:
+        print("Правильно!")
+        score += 1000
+        current_question += 1
+    else:
+        if current_question == len(questions):
+            print(f'У нас есть победитель! Он забирает {score}$')
+        else:
+            print(f'А вот и нет! К сожалению ты потерял все деньги(')
+
+
+play_game(questions)
